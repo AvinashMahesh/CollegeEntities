ec899a09365a871b99bb1353bbf0314830971724
Avinash Mahesh <87287679+AvinashMahesh@users.noreply.github.com>
2021-10-21 02:28:54 -0400

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -16,6 +16,14 @@ public class Teacher extends Person {
 		return yearsExp;
 	}
 	
+	@Override
+	public boolean isEmployable() {
+		if(this.getAge() > 22 && this.getYears() >= 5)
+			return true;
+			else
+		return false;
+	}
+	
 	public String toString() {
 		return super.getName() + " teaches " + subject + " and has " +yearsExp + " years of experience.";
 	}
